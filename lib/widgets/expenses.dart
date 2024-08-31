27dd8a64867ff4a7a891c7532e0f1eecba8e175c
JaimeFigueroaP22 <jfigueroap@utem.cl>
2024-08-31 10:18:53 -0400

    cambiolenguaje

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -17,13 +17,13 @@ class Expenses extends StatefulWidget {
 class _ExpensesState extends State<Expenses> {
   final List<Expense> _registeredExpenses = [
     Expense(
-      title: 'Flutter Course',
+      title: 'Curso de Flutter',
       amount: 19.99,
       date: DateTime.now(),
       category: Category.work,
     ),
     Expense(
-      title: 'Cinema',
+      title: 'Cine',
       amount: 15.69,
       date: DateTime.now(),
       category: Category.leisure,
@@ -54,9 +54,9 @@ class _ExpensesState extends State<Expenses> {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         duration: const Duration(seconds: 3),
-        content: const Text('Expense deleted.'),
+        content: const Text('Gasto eliminado.'),
         action: SnackBarAction(
-          label: 'Undo',
+          label: 'Deshacer',
           onPressed: () {
             setState(() {
               _registeredExpenses.insert(expenseIndex, expense);
@@ -72,7 +72,7 @@ class _ExpensesState extends State<Expenses> {
     final width = MediaQuery.of(context).size.width;
 
     Widget mainContent = const Center(
-      child: Text('No expenses found. Start adding some!'),
+      child: Text('No se encontraron gastos. ¡Empieza a agregar algunos!'),
     );
 
     if (_registeredExpenses.isNotEmpty) {
@@ -84,7 +84,7 @@ class _ExpensesState extends State<Expenses> {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter ExpenseTracker'),
+        title: const Text('Mi Billetera'),
         actions: [
           IconButton(
             onPressed: _openAddExpenseOverlay,
