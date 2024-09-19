868f23918748fa3525e6fe01b039e30a0f927956
calcifer <caleb.calfulaf@hfsolutions.cl>
2024-09-19 12:15:24 -0300

    add new screen

---
+++ lib/screens/filtros.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+class FiltersScreen extends StatelessWidget {
+  const FiltersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Filters'),
+      ),
+      body: Column(
+        children: [
+          SwitchListTile(
+            value: false, // Valor inicial sin función
+            onChanged: (isChecked) {
+              // No hay ninguna acción aquí
+            },
+            title: Text(
+              'Comida',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            subtitle: Text(
+              'Solo incluye gastos relacionados con comida.',
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
+          ),
+          SwitchListTile(
+            value: false, // Valor inicial sin función
+            onChanged: (isChecked) {
+              // No hay ninguna acción aquí
+            },
+            title: Text(
+              'Viajes',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            subtitle: Text(
+              'Solo incluye gastos relacionados con viajes.',
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
+          ),
+          SwitchListTile(
+            value: false, // Valor inicial sin función
+            onChanged: (isChecked) {
+              // No hay ninguna acción aquí
+            },
+            title: Text(
+              'Ocio',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            subtitle: Text(
+              'Solo incluye gastos relacionados con ocio.',
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
+          ),
+          SwitchListTile(
+            value: false, // Valor inicial sin función
+            onChanged: (isChecked) {
+              // No hay ninguna acción aquí
+            },
+            title: Text(
+              'Trabajo',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            subtitle: Text(
+              'Solo incluye gastos relacionados con trabajo.',
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
+          ),
+          SwitchListTile(
+            value: false, // Valor inicial sin función
+            onChanged: (isChecked) {
+              // No hay ninguna acción aquí
+            },
+            title: Text(
+              'Otros',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            subtitle: Text(
+              'Solo incluye otros tipos de gastos.',
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
+          ),
+        ],
+      ),
+    );
+  }
+}
