1f1fcea91d1605cf665f2487873b70a0d49bd00d
Caleb <ezequiel.calfulaf.m@gmail.com>
2025-12-06 14:40:45 -0300

    aqui se maneja el ftu, para debugear si es true va aparecer siempre

--- lib/core/ftu_config/ftu_config.dart
+++ lib/core/ftu_config/ftu_config.dart
@@ -1,3 +1,3 @@
 // FTU (First Time User) feature flags for debugging/testing
 // Set `kForceFTU` to true to force-show FTU flows during development.
-const bool kForceFTU = true;
+const bool kForceFTU = false;
